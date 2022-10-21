968668428610474cb83d2d5c55dddf7441ed6296
el-hadj <elhadj.balde@etu.u-pec.fr>
2022-10-22 00:54:26 +0200

    test

---
+++ prototypeApi/src/main/java/com/smartBuilding/prototypeApi/PrototypeApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartBuilding.prototypeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeApiApplication.class, args);
+	}
+
+}
