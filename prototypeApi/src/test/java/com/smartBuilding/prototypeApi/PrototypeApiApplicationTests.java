0e952ab8569486807d04338b9afd1d5053de30bd
el-hadj <elhadj.balde@etu.u-pec.fr>
2022-10-22 00:47:10 +0200

    test

---
+++ prototypeApi/src/test/java/com/smartBuilding/prototypeApi/PrototypeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartBuilding.prototypeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
